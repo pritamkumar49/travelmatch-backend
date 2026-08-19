239f8ee45b0cc30b54319c522d39f03d7e0a9445
pritamkumar49 <pritamkumaruemlab@gmail.com>
2026-08-19 21:29:21 +0530

    Update AppConfig.java

--- src/main/java/com/pvp/travelmatch/config/AppConfig.java
+++ src/main/java/com/pvp/travelmatch/config/AppConfig.java
@@ -1,5 +1,6 @@
 package com.pvp.travelmatch.config;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.client.RestTemplate;
@@ -8,8 +9,12 @@ import org.springframework.web.client.RestTemplate;
 public class AppConfig {
 
     @Bean
-    public RestTemplate restTemplate(){
+    public RestTemplate restTemplate() {
         return new RestTemplate();
     }
 
+    @Bean
+    public ObjectMapper objectMapper() {
+        return new ObjectMapper();
+    }
 }
